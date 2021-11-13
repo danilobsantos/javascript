4e176e3c7bea3a0c8a2456eaa53400534c9a1660
Danilo Santos <49441603+danilobsantos@users.noreply.github.com>
2021-11-13 16:07:44 -0300

    Exercícios para o feriado

---
+++ exercicios.js
@@ -0,0 +1,13 @@
+/* Faça o exercícios abaixo:
+
+1 – Crie um script que calcule a média de 3 notas digitadas. 
+
+2 – Crie um script que receba cinco números e imprima o quadrado de cada número.
+
+3 – Crie um script que receba 5 números e verifique qual é o maior e exiba-o.
+
+4 – Crie um script que receba várias idades. Exiba quantas pessoas são maior de idade (18 anos) e quantas são menores.
+
+5 – Crie um script que receba dados de altura e sexo de 5 pessoas. Imprima na tela quantas pessoas são do sexo masculino e quantas pessoas são do sexo feminino.
+
+6 – Crie um script que receba strings com vários nomes. Exiba na tela todos os nomes digitados, porém de maneira invertida (do último para o primeiro). *\
